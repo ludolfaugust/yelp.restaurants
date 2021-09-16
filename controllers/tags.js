e2b9c7f33fb97c1fa18ec21609d6f0e6b0cbea79
ludo <85556848+ludolfaugust@users.noreply.github.com>
2021-09-16 17:09:57 +0200

    add tag

---
+++ controllers/tags.js
@@ -0,0 +1,35 @@
+// const Tag = require("../models/Tag");
+
+// const getTags = async (req, res) => {
+//   try {
+//     const cities = await Cities.find();
+//     console.log(cities);
+//     res.json({
+//       success: true,
+//       data: cities,
+//       msg: "show all cities",
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// const getCity = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const city = await City.findById(id);
+
+//     res.json({
+//       msg: `show city with id ${id}`,
+//       success: true,
+//       data: city,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// module.exports = {
+//   getCities,
+//   getCity,
+// };
